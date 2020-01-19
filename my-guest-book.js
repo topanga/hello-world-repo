61b43cba33d824a677e221fa91fd3d22f40567ef
topanga <mdy@onpick.com>
2020-01-19 02:03:46 -0500

    my guestbook commit 1

---
+++ my-guest-book.js
@@ -0,0 +1,22 @@
+console.log('starting my log book');
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({region:'us-east-1'});
+
+exports.handle = function(e, ctx, callback) {
+
+    var params = {
+        Item : {
+            posted : Date.now(),
+            post : "A user post"
+        },
+        TableName : 'MyGuestBookDynDbTable'
+    }
+
+    docClient.put(params, function (err, data) {
+        if (err) {
+            callback(err, null);
+        } else {
+            callback(null, data);
+        }
+    });
+}
